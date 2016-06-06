85cd11821113aced5d1c01f519d132abbc8a1612
Arthur Xavier <arthur.xavierx@gmail.com>
2016-06-06 19:16:36 -0300

    Change BookAdapter to lazily load books by extending CursorAdapter

--- app/src/main/java/br/ufmg/dcc052/oncebook/book/BookAdapter.java
+++ app/src/main/java/br/ufmg/dcc052/oncebook/book/BookAdapter.java
@@ -1,38 +1,27 @@
 package br.ufmg.dcc052.oncebook.book;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
+import android.widget.CursorAdapter;
 
 /**
  * Created by xavier on 6/6/16.
  */
-public class BookAdapter extends BaseAdapter {
+public class BookAdapter extends CursorAdapter {
 
-  private Context context;
-
-  public BookAdapter(Context c) {
-    this.context = c;
+  public BookAdapter(Context context) {
+    super(context, new SQLiteBookRepository(context).getAllCursor());
   }
 
   @Override
-  public int getCount() {
-    return 0;
-  }
-
-  @Override
-  public Object getItem(int position) {
+  public View newView(Context context, Cursor cursor, ViewGroup parent) {
     return null;
   }
 
   @Override
-  public long getItemId(int position) {
-    return 0;
-  }
+  public void bindView(View view, Context context, Cursor cursor) {
 
-  @Override
-  public View getView(int position, View convertView, ViewGroup parent) {
-    return null;
   }
 }
