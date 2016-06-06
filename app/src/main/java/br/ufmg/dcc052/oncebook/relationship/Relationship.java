c98d6a138b6cdf2e7f200649acb91f78fe790f2d
Arthur Xavier <arthur.xavierx@gmail.com>
2016-06-06 20:22:34 -0300

    Change Relationship model to support only binary relationships

--- app/src/main/java/br/ufmg/dcc052/oncebook/relationship/Relationship.java
+++ app/src/main/java/br/ufmg/dcc052/oncebook/relationship/Relationship.java
@@ -1,16 +1,24 @@
 package br.ufmg.dcc052.oncebook.relationship;
 
-import br.ufmg.dcc052.oncebook.character.*;
+import br.ufmg.dcc052.oncebook.character.Character;
 
 /**
  * Created by xavier on 6/6/16.
  */
-public abstract class Relationship {
+public class Relationship {
 
   private String name;
+  private Character firstCharacter;
+  private Character secondCharacter;
 
   public Relationship(String name) {
     this.name = name;
+  }
+
+  public Relationship(String name, Character firstCharacter, Character secondCharacter) {
+    this(name);
+    this.firstCharacter = firstCharacter;
+    this.secondCharacter = secondCharacter;
   }
 
   public String getName() {
@@ -20,5 +28,17 @@ public abstract class Relationship {
     this.name = name;
   }
 
-  public abstract br.ufmg.dcc052.oncebook.character.Character[] getCharacters();
+  public Character getFirstCharacter() {
+    return firstCharacter;
+  }
+  public void setFirstCharacter(Character firstCharacter) {
+    this.firstCharacter = firstCharacter;
+  }
+
+  public Character getSecondCharacter() {
+    return secondCharacter;
+  }
+  public void setSecondCharacter(Character secondCharacter) {
+    this.secondCharacter = secondCharacter;
+  }
 }
