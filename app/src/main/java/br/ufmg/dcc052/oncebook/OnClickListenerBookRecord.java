7b81a0f45b101c62ab7e88cdd3dd6bc47925b5dd
Jota Junior <jotavrj@gmail.com>
2016-06-10 14:04:56 -0300

    New listener

---
+++ app/src/main/java/br/ufmg/dcc052/oncebook/OnClickListenerBookRecord.java
@@ -0,0 +1,20 @@
+package br.ufmg.dcc052.oncebook;
+
+import android.view.View;
+
+/**
+ * Created by jotajunior on 6/10/16.
+ */
+public class OnClickListenerBookRecord implements View.OnClickListener {
+  private MainActivity ma;
+
+  public OnClickListenerBookRecord() {}
+  public OnClickListenerBookRecord(MainActivity ma) {
+    this.ma = ma;
+  }
+  
+  @Override
+  public void onClick(View view) {
+
+  }
+}
