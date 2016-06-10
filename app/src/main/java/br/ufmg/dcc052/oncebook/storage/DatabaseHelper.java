e6162f71a161173d8900db8fa187952ccc2485f6
Arthur Xavier <arthur.xavierx@gmail.com>
2016-06-09 22:33:13 -0300

    Fix SQL for database creation

--- app/src/main/java/br/ufmg/dcc052/oncebook/storage/DatabaseHelper.java
+++ app/src/main/java/br/ufmg/dcc052/oncebook/storage/DatabaseHelper.java
@@ -16,11 +16,8 @@ public class DatabaseHelper extends SQLiteOpenHelper {
   private static final String DATABASE_NAME = "oncebook.db";
   private static final int DATABASE_VERSION = 1;
 
-  private final Context context;
-
   public DatabaseHelper(Context context) {
     super(context, DATABASE_NAME, null, DATABASE_VERSION);
-    this.context = context;
   }
 
   @Override
@@ -36,7 +33,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
   }
 
   private String createBooksTable() {
-    return "CREATE TABLE " + SQLiteBookRepository.TABLE_NAME + " (" +
+    return "CREATE TABLE IF NOT EXISTS " + SQLiteBookRepository.TABLE_NAME + " (" +
       SQLiteBookRepository.COLUMN_NAME_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
       SQLiteBookRepository.COLUMN_NAME_NAME + " TEXT, " +
       SQLiteBookRepository.COLUMN_NAME_DESCRIPTION + " TEXT" +
@@ -44,7 +41,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
   }
 
   private String createCharactersTable() {
-    return "CREATE TABLE " + SQLiteCharacterRepository.TABLE_NAME + " (" +
+    return "CREATE TABLE IF NOT EXISTS " + SQLiteCharacterRepository.TABLE_NAME + " (" +
       SQLiteCharacterRepository.COLUMN_NAME_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
       SQLiteCharacterRepository.COLUMN_NAME_NAME + " TEXT, " +
       SQLiteCharacterRepository.COLUMN_NAME_DESCRIPTION + " TEXT, " +
@@ -52,21 +49,21 @@ public class DatabaseHelper extends SQLiteOpenHelper {
       SQLiteCharacterRepository.COLUMN_NAME_APPEARANCEPAGE + " INTEGER, " +
       SQLiteCharacterRepository.COLUMN_NAME_PICTURE + " BLOB, " +
       "FOREIGN KEY (" + SQLiteCharacterRepository.COLUMN_NAME_BOOK + ") REFERENCES " +
-        SQLiteBookRepository.TABLE_NAME + ".(" + SQLiteBookRepository.COLUMN_NAME_ID + ")" +
+        SQLiteBookRepository.TABLE_NAME + "(" + SQLiteBookRepository.COLUMN_NAME_ID + ")" +
       ")";
   }
 
   private String createRelationshipsTable() {
-    return "CREATE TABLE " + SQLiteRelationshipRepository.TABLE_NAME + " (" +
+    return "CREATE TABLE IF NOT EXISTS " + SQLiteRelationshipRepository.TABLE_NAME + " (" +
       SQLiteRelationshipRepository.COLUMN_NAME_FISRTCHARACTER + " INTEGER, " +
       SQLiteRelationshipRepository.COLUMN_NAME_SECONDCHARACTER + " INTEGER, " +
       SQLiteRelationshipRepository.COLUMN_NAME_NAME + " TEXT, " +
       "PRIMARY KEY (" + SQLiteRelationshipRepository.COLUMN_NAME_FISRTCHARACTER + ", " +
         SQLiteRelationshipRepository.COLUMN_NAME_SECONDCHARACTER + "), " +
       "FOREIGN KEY (" + SQLiteRelationshipRepository.COLUMN_NAME_FISRTCHARACTER + ") REFERENCES " +
-        SQLiteCharacterRepository.TABLE_NAME + ".(" + SQLiteCharacterRepository.COLUMN_NAME_ID + "), " +
+        SQLiteCharacterRepository.TABLE_NAME + "(" + SQLiteCharacterRepository.COLUMN_NAME_ID + "), " +
       "FOREIGN KEY (" + SQLiteRelationshipRepository.COLUMN_NAME_SECONDCHARACTER + ") REFERENCES " +
-        SQLiteCharacterRepository.TABLE_NAME + ".(" + SQLiteCharacterRepository.COLUMN_NAME_ID + ")" +
+        SQLiteCharacterRepository.TABLE_NAME + "(" + SQLiteCharacterRepository.COLUMN_NAME_ID + ")" +
       ")";
   }
 }
